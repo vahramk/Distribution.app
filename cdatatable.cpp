0b354e9dddaa1f54bf3bed6383a06d50b04e2e1d
oxt <ogsimxan@yahoo.com>
2014-10-28 00:58:49 +0400

    added outline of Data table class implementation.

---
+++ cdatatable.cpp
@@ -0,0 +1,15 @@
+/* 
+* @file cdatatable.h
+* @brief Data table class implementation.
+*
+**/
+
+#include "cdatacolumn.h"
+#include "cdatatable.h"
+
+#include <string>
+
+namespace data_layer
+{
+// ......
+} //namespace data_layer
