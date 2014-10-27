c5436a690d852c32103754c2dd54aeb1af9ae5e3
oxt <ogsimxan@yahoo.com>
2014-10-28 00:21:03 +0400

    Added IDataColumn interface class
    Not final implementation.

--- idatacolumn.h
+++ idatacolumn.h
@@ -1,1 +1,37 @@
-# Data column class - implements IDataColumn interface
+/* 
+*@brief Data column class - implements IDataTable interface
+*
+**/
+
+#ifndef IDATACOLUMN_H
+#define IDATACOLUMN_H
+
+#include <string>
+
+namespace data_layer 
+{
+
+enum EDataType { 
+		Int = 0,
+		Double,
+		String,
+		DateTime,
+		TypeCount 
+};
+
+class IDataColumn 
+{
+public:
+	virtual size_t getSize() const = 0;
+	virtual const std::string& getName() const = 0;
+	virtual const EDataType getType() const = 0;
+	virtual void getData(size_t row, std::string& val) const = 0;
+	virtual void getData(size_t row, time_t& val) const = 0;
+	virtual void getData(size_t row, int& val) const = 0;
+	virtual void getData(size_t row, double& val) const = 0;
+	virtual ~IDataColumn() = 0{};
+};
+
+} //namespace data_layer
+
+#endif //IDATACOLUMN_H
