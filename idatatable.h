d2361364893361ba2f4a8137289d9db193c25735
oxt <ogsimxan@yahoo.com>
2014-10-28 00:24:28 +0400

    Added IDataTable interface class

--- idatatable.h
+++ idatatable.h
@@ -1,1 +1,28 @@
-# Data table class - implements IDataTable interface
+/* 
+*@brief Data column class - implements IDataTable interface
+*
+**/
+
+#ifndef IDATATABLE_H
+#define IDATATABLE_H
+
+#include "idatacolumn.h"
+
+#include <string>
+
+namespace data_layer 
+{
+
+	class IDataTable 
+	{
+	public:
+		virtual const std::string& getColumnName(size_t index) const = 0;
+		virtual size_t getColumnCount() const = 0;
+		virtual size_t getRowCount() const = 0;
+		virtual const IDataColumn* getColumn(size_t index)const = 0 ;
+		virtual ~IDataTable(){};
+	};
+
+} //namespace data_layer
+
+#endif //IDATATATABLE_H
