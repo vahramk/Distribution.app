8e05b9ecea2a9569c5555f01eb944c786f5962fd
oxt <ogsimxan@yahoo.com>
2014-10-28 01:00:47 +0400

    added outline of Data Column class implementation.

---
+++ cdatacolumn.cpp
@@ -0,0 +1,14 @@
+/* 
+* @file cdatacolumn.h
+* @brief Data column class implementation.
+*
+**/
+
+#include "cdatacolumn.h"
+#include <deque>
+#include <cassert>
+
+namespace data_layer
+{
+// ....
+} //namespace data_layer
