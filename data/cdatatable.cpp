b37fa53a44db5eca32677b398828585255af06df
oxt <ogsimxan@yahoo.com>
2014-10-30 01:02:42 +0400

    added CDataColumn class. Added detailed doxygen comments.

--- data/cdatatable.cpp
+++ data/cdatatable.cpp
@@ -1,15 +1,99 @@
 /* 
 * @file cdatatable.h
-* @brief Data table class implementation.
+* @brief Data table class - implements CDataTable interface
 *
 **/
 
-#include "cdatacolumn.h"
-#include "cdatatable.h"
+#ifndef CDATATABLE_H
+#define CDATATABLE_H
 
-#include <string>
+#include "idatatable.h"
 
-namespace data_layer
-{
-// ......
-} //namespace data_layer
+#include <boost/interprocess/smart_ptr/unique_ptr.hpp>
+#include <boost/interprocess/containers/vector.hpp>
+
+namespace data
+{	
+	class CDataColumn;
+	
+	//unnamed namespace
+	namespace {
+		/* 
+		* @brief This is a generic deleter for 
+		*		unique_ptr.
+		**/
+		template<typename T> 
+		struct GenericDeleter 
+		{
+			void operator()(T *p)
+			{
+				delete p;
+			}
+		};
+
+		//This is the type of a unique pointer.
+		typedef boost::interprocess::unique_ptr<CDataColumn, GenericDeleter<CDataColumn> > unique_ptr_type;
+
+		//Defines container of unique pointer.
+		typedef boost::interprocess::vector< unique_ptr_type > unique_ptr_vector_t;
+
+	} //unnamed namespace
+
+	/* 
+	* @brief The class CDataTable implements the
+	*		IDataTable interface.
+	**/
+	class CDataTable:public IDataTable
+	{
+	public:
+		/* 
+		* @brief This is the constructor.
+		**/
+		CDataTable();
+
+		/* 
+		* @brief This function returns the name of the column, 
+		*		 corresponding to passed arguments.
+		* @param[in] index is the column index in table. 
+		* @exception throws std::out_of_range exception.
+		**/
+		virtual const std::string& getColumnName(size_t index) const;
+
+		/* 
+		* @brief This function returns the count of columns in table.
+		**/
+		virtual size_t getColumnCount() const;
+
+		/* 
+		* @brief This function returns the count of rows in table.
+		**/
+		virtual size_t getRowCount() const;
+
+		/* 
+		* @brief This function returns the column pointer, 
+		*		 corresponding to passed arguments.
+		* @param[in] index is the column index in table. 
+		* @exception throws std::out_of_range exception.
+		**/
+		virtual const IDataColumn* getColumn(size_t index) const;
+
+		/* 
+		* @brief This is the desconstructor.
+		**/
+		virtual ~CDataTable();
+		
+		/* 
+		* @brief This function adds new column into current table, 
+		*		 corresponding to passed arguments.
+		* @param[in] column is pointer to column. 
+		* @exception throws std::out_of_range exception.
+		**/		
+		void addDataColumn(CDataColumn* column);
+
+	private:
+		unique_ptr_vector_t m_data;
+	};
+
+} //namespace data
+
+#endif //CDATATABLE_H
