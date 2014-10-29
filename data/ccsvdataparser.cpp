7523f8ccaccd2b48e0dba66702507c790bf04e47
oxt <ogsimxan@yahoo.com>
2014-10-30 01:07:47 +0400

    added this files to seporate svc parser implementation.

---
+++ data/ccsvdataparser.cpp
@@ -0,0 +1,12 @@
+/* 
+* @file cdataparser.cpp
+* @brief Implementation of CSV Data parser for the DataTable.
+*
+**/
+
+#include "ccsvdataparser.h"
+
+namespace data
+{
+
+} //namespace data
---
+++ data/ccsvdataparser.h
@@ -0,0 +1,16 @@
+/* 
+* @file cdataparser.h
+* @brief CSV Data parser interface for the DataTable.
+*
+**/
+
+#include "cdataparser.h"
+
+namespace data
+{
+	class CCSVParser: public CDataParser
+	{
+	public:
+	};
+
+} //namespace data
