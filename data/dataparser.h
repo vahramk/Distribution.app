244de97491610321348971cde933443a6086c6e9
oxt <ogsimxan@yahoo.com>
2014-10-30 01:04:27 +0400

    added outline for CDataParser class.

--- data/dataparser.h
+++ data/dataparser.h
@@ -1,1 +1,19 @@
-# CSV Data parser for the DataTable
+/* 
+* @file cdataparser.h
+* @brief Data parser interface for the DataTable.
+*
+**/
+
+namespace data
+{
+	/* 
+	* @brief The class CDataParser implements
+	*		parser functionality of data layer.
+	**/
+	class CDataParser
+	{
+	public:
+		
+	};
+
+} //namespace data
