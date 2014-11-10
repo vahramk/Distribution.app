771f3468877c0b92066739c6bd88851eb47f0c1c
oxt <ogsimxan@yahoo.com>
2014-11-11 02:35:44 +0400

    Removed unused variable.

--- data/datautility.cpp
+++ data/datautility.cpp
@@ -11,7 +11,6 @@ namespace data
 	//fixed format of data time!
 	void fromStringToTime(const std::string& value, struct tm& dt) 
 	{
-		char buf[20];
 		sscanf(value.c_str(), "%d-%d-%d %d:%d:%d", &dt.tm_year, &dt.tm_mon, &dt.tm_mday, &dt.tm_hour, &dt.tm_min, &dt.tm_sec);
 	}
 
