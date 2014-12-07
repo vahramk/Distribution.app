bf456fd36047298ab2b0b71acd4b1acb03715e6e
tigrangs <programtg@yahoo.com>
2014-12-07 16:45:35 +0400

    adding CDataArea
    CDataArea class translate data area pointes to screen points

---
+++ chart/cdataarea.cpp
@@ -0,0 +1,23 @@
+#include "cdataarea.h"
+
+CDataArea::CDataArea(const QRectF& sc)
+{
+    m_oscreenrect = sc;
+}
+
+void CDataArea::init(const QPointF &tl, const QPointF &br)
+{
+    m_odatarect = QRectF(tl, br);
+}
+
+QPointF CDataArea::translate(const QPointF &p)
+{
+    double x = m_oscreenrect.x() * p.x() / m_odatarect.x();
+    double y = m_oscreenrect.y() * p.y() / m_odatarect.y();
+    return QPointF(x, y);
+}
+
+void CDataArea::rescale(const QRectF &sc)
+{
+    m_oscreenrect = sc;
+}
---
+++ chart/cdataarea.h
@@ -0,0 +1,49 @@
+#ifndef CDATAAREA_H
+#define CDATAAREA_H
+
+/**
+* @file cdataarea.h
+* @brief CDataArea class interface.
+*/
+
+#include <QPointF>
+#include <QRectF>
+
+class CDataArea
+{
+public:
+    /**
+     * @brief constracter for given screen
+     * @param[in] sc rect of given screen
+     */
+    CDataArea(const QRectF& sc);
+
+public:
+    /**
+     * @brief initialize data area
+     * @param[in] tl top left point of the rect
+     * @param[in] br bottom right point of the rect
+     */
+    void init(const QPointF& tl, const QPointF& br);
+
+public:
+    /**
+     * @brief translate given point from data area to paint devise
+     * @param[in] p point in data area
+     * @return translated point
+     */
+    QPointF translate(const QPointF& p);
+
+public:
+    /**
+     * @brief rescale to given screen
+     * @param[in] sc new screen
+     */
+    void rescale(const QRectF& sc);
+
+private:
+    QRectF m_odatarect;
+    QRectF m_oscreenrect;
+};
+
+#endif // CDATAAREA_H
