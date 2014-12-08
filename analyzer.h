6d7be0a69890e6ca3ac95a3026e30aeda01d90c1
artarz <artashes.arzumanyan@gmail.com>
2014-12-08 15:39:46 +0400

    analyzer and parameterstore are changed
    analyzer and parameterstore are changed

--- analyzer.h
+++ analyzer.h
@@ -1,2 +1,24 @@
-# Distribution analyzer class
-# Analyzes selected parameters and keeps results
+//# Distribution analyzer class
+//# Analyzes selected parameters and keeps results
+
+#ifndef CANALYZER_H
+#define CANALYZER_H
+
+#include "data/idatatable.h"
+#include "parameterstore.h"
+
+class CAnalalyzer: public data::IDataTable
+{
+public:
+	
+	CAnalalyzer (const IDataTable*, CParameterStore* );
+	void analyze();
+
+private:
+
+	void CountMin();
+	void ConutMAx();
+	
+	const IDataTable* m_pIdataTbl;
+};
+#endif // CANALYZER_H
--- parameterstore.h
+++ parameterstore.h
@@ -1,1 +1,13 @@
-# Store of analyzed results of parameters - implemnents IDataTable interface
+//# Store of analyzed results of parameters - implemnents IDataTable interface
+//#include "data/idatatable.h"
+
+#ifndef CPARAMETER_H
+#define CPARAMETER_H
+
+#include "data/idatatable.h"
+
+class CParameterStore: public data::IDataTable
+{
+
+};
+#endif // CCPARAMETER_H
