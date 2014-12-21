f29f51a136b7dd85c52b19a209928d3f5df8ddd0
oxt <ogsimxan@yahoo.com>
2014-12-22 01:21:08 +0400

    Added interface for CFuntion.

--- core/cfunction.cpp
+++ core/cfunction.cpp
@@ -3,3 +3,19 @@
 * @brief Implementation of the CFunction cless.
 *
 */
+
+#include "cfunction.hpp"
+#include <data/idatacolumn.h>
+//include the Analyser project.
+
+namespace core
+{
+
+    void CFunction::configur(QList<CFunction::EType> )
+    {
+        //implementation to be added.
+    }
+
+    //ParamInfo CFunction::compute(IDataColumn* c) {}
+
+} //namespace core
