333e4aa0439715f550d2576e4cbf3922d1b33ac7
oxt <ogsimxan@yahoo.com>
2014-12-21 23:03:06 +0400

    Changed interface std to QString, QVector.

--- data/ccsvdataparser.h
+++ data/ccsvdataparser.h
@@ -10,8 +10,10 @@
 #include "datautility.h"
 #include "idataparsermodule.h"
 
-#include <string>
 #include <vector>
+
+#include <QString>
+#include <QVector>
 
 namespace data
 {
@@ -32,7 +34,7 @@ namespace data
 		* @brief This is the constructor.
 		* @param[in] file_name is the path which shell parsed. 
 		*/
-		CCSVParser(const std::string& file_name);
+        CCSVParser(const QString& file_name);
 		
 		/** 
 		* @brief This is the main parser function.
@@ -42,31 +44,28 @@ namespace data
 		/** 
 		* @brief This is the deconstructor.
 		*/
-		virtual ~CCSVParser()
-		{
-			delete m_table;
-		}
+        virtual ~CCSVParser();
 
 	private:
 		/** 
 		* @brief This function returns the type of the passed arguments.
 		* @param[in] value is the data. 
 		*/
-		EDataType get_type(const std::string& value) const;
+        EDataType get_type(const QString& value) const;
 		
 		/** 
 		* @brief This function tokenizes the passed string argument.
 		* @param[in] line is the string to be tokenized. 
 		* @param[out] vec is the container which will be returned.
 		*/
-		void tokenize_line(const Tockenizer& line, std::vector<std::string>& vec) const;
+        void tokenize_line(const Tockenizer& line, QVector<QString>& vec) const;
 		
 		/** 
 		* @brief This function adds value to column corresponding to passed arguments.
 		* @param[in] c is the column. 
 		* @param[in] type is the value.
 		*/
-		void addValueToColumn(CDataColumn* c, const std::string& value);
+        void addValueToColumn(CDataColumn* c, const QString& value);
 		
 		/** 
 		* @brief This function adds value to column of current table corresponding to passed arguments.
@@ -74,10 +73,10 @@ namespace data
 		* @param[in] i is the index of the column.
 		* @param[in] value is the value.
 		*/
-		void addValueToTable(CDataTable* t, size_t i,const std::string& value);
+        void addValueToTable(CDataTable* t, int i,const QString& value);
 
 	private:
-		const std::string m_path;
+        const QString m_path;
 		CDataTable* m_table;
 	};
 
